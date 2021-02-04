2992f48ac0da66bdcdafee9324ecdd5ed23a774a
Анна Абакумова <49556500+pin0k@users.noreply.github.com>
2021-02-04 08:48:31 +0300

    Обновила функции, убрала лишние скобки и return

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
-const getRandomInteger = (min, max) => {return Math.floor(Math.random() * (max - min + 1)) + min};
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 // Функция для проверки максимальной длины строки. Кекстаграм
-const getValidationObject = (checkedObject, maxValue) => {return (checkedObject.length < maxValue) ? true : false;};
+const getValidationValue = (checkedValue, maxValue) => (checkedValue.length < maxValue) ? true : false;
