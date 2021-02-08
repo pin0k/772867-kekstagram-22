272cddc0a8647414cae1608e1b0e457a2ac0b5d0
Анна <pin0k@yandex.ru>
2021-02-08 06:47:18 +0300

    Исправила ошибка линтера

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-// Функция для проверки максимальной длины строки. Кекстаграм
-const getValidationValue = (checkedValue, maxValue) => (checkedValue.length < maxValue) ? true : false;
-
 //Имена авторов комментариев
 const NAMES = [
   'Иван',
@@ -30,7 +27,7 @@ const NAMES = [
   'Эльвира',
   'Никита',
   'Эсмеральда',
-  'Платон'
+  'Платон',
 ];
 
 //Текст для формирования комментария
@@ -40,7 +37,7 @@ const COLLECTION_TEXT = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
 //Функция, создающая один объект
@@ -56,11 +53,11 @@ const createElement = () => {
         id: String(numberElement) + getRandomInteger(0, 25),
         avatar: 'img/avatar' + getRandomInteger(0, 6) + '.svg',
         message: COLLECTION_TEXT[getRandomInteger(0, COLLECTION_TEXT.length)],
-        name: NAMES[getRandomInteger(0, NAMES.length)]
-       }
-    })
+        name: NAMES[getRandomInteger(0, NAMES.length)],
+      }
+    }),
   };
 };
 
 //Создание массива объектов
-const readyArray = new Array(25).fill(null).map(() => createElement());
+new Array(25).fill(null).map(() => createElement());
