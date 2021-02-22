479252e31e3bec75ef745597139b66d182ae3893
Pinok <pin0k@yandex.ru>
2021-02-22 15:11:00 +0300

    поиск порядкового номера элемента в foreach

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -11,13 +11,13 @@ const template = templateFragment.querySelector('div');
 const fragment = document.createDocumentFragment();
 
 
-createDataList.forEach((element) => {
+createDataList.forEach((element, index) => {
   const newThumbnail = template.cloneNode(true);
 
   newThumbnail.querySelector('img').src = element.url;
   newThumbnail.querySelector('.picture__comments').textContent = element.likes;
   //newThumbnail.querySelector('.picture__likes').textContent = element.comments[getRandomInteger(1,6)].message;
-  console.log(element.comments.)
+  console.log(element.comments[index]);
 
   fragment.appendChild(newThumbnail);
 });
